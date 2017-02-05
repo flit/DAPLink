b581ecfac5199e0dcae00ab1e9ae5df552fd2450
Chris Reed <chris.reed@nxp.com>
2017-02-04 19:18:36 -0600

    Changed FRDM-KL28ZEM volume name to be more identifiable.

--- source/board/frdmkl28zem.c
+++ source/board/frdmkl28zem.c
@@ -27,10 +27,10 @@ const char *board_id = "0215";
 //
 // URL_NAME and DRIVE_NAME must be 11 characters excluding
 // the null terminated character
-// Note - 4 byte alignemnt required as workaround for ARMCC compiler bug with weak references
+// Note - 4 byte alignment required as workaround for ARMCC compiler bug with weak references
 __attribute__((aligned(4)))
 const vfs_filename_t daplink_url_name =       "PRODINFOHTM";
 __attribute__((aligned(4)))
-const vfs_filename_t daplink_drive_name =     "FRDM-KL28ZD";
+const vfs_filename_t daplink_drive_name =     "FRDM-KL28EM";
 __attribute__((aligned(4)))
 const char *const daplink_target_url = "http://www.nxp.com/frdm-kl28zem";
